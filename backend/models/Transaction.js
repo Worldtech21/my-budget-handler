0bb986e82599ce0fe2465f7cf85c4e4470e929e3
Worldtech21 <113043988+Worldtech21@users.noreply.github.com>
2025-02-09 21:20:04 +0530

    Create Transaction.js

---
+++ backend/models/Transaction.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const TransactionSchema = new mongoose.Schema({
+  text: { type: String, required: true },
+  amount: { type: Number, required: true },
+  type: { type: String, enum: ["income", "expense"], required: true },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model("Transaction", TransactionSchema);
